bdca3e4fd335ecdf3cafca27876f11cd0a063e08
blukasho <bodik1w@gmail.com>
2019-07-15 11:09:48 +0300

    mod

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 12:48:53 by blukasho          #+#    #+#             */
-/*   Updated: 2019/07/14 11:18:25 by blukasho         ###   ########.fr       */
+/*   Updated: 2019/07/15 10:53:04 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 int			main(void)
 {
-	char	*buf;
-	int		fd;
+	long long	l;
 
-	fd = open("./out", O_RDWR);
-	get_next_line(STDOUT_FILENO, &buf);
-	ft_putendl_fd(buf, fd);
+	l = ~0;
+	++l;
+	if (l)
+	{}
+//	filler_read();
 	return (0);
 }
